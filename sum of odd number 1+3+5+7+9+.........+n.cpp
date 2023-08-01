d240360eb931fc7638cdd3b4cbb0abe90e903bd6
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-01 23:56:31 +0600

    Add files via upload

---
+++ sum of odd number 1+3+5+7+9+.........+n.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+   int num;
+   int sum=0;
+   cout<<"enter your number :";
+   cin>>num;
+   for(int i=1;i<=num;i=2+i)
+   {
+       cout<<"+"<<i ;
+
+       sum=sum+i;
+       }
+       cout<<endl;
+
+    cout<<"sum is="<<sum;
+
+
+    return 0;
+
+}
+
+
+
