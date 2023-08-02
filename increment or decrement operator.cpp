7eb252ff989bf3c0c662a7acd0f4064b152522f4
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-02 23:47:22 +0600

    Add files via upload

---
+++ increment or decrement operator.cpp
@@ -0,0 +1,19 @@
+
+#include<iostream>
+#include<conio.h>
+using namespace std;
+int main()
+{
+      int x=5;
+      int y= ++x;
+
+
+        cout<< "result of y:"<<y<<endl;
+        cout<< "result of x :"<<x<<endl;
+
+
+
+    getch();
+
+
+}
