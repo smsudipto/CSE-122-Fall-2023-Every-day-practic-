754af255609b8fd74b23419c9e89e86cf9cfc915
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-05 11:00:06 -0700

    Add files via upload

---
+++ address of integer using pointer.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int Dipto=20;
+    int *p;
+    p=&Dipto;
+    cout<<Dipto<<endl;
+    cout<<&p<<endl;
+     cout<<p<<endl;
+    cout<<*p<<endl;
+
+
+}
