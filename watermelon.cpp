50a50fcd9e3eafb51a572fc3e19396fde6af070f
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-06 00:01:59 -0700

    Add files via upload

---
+++ watermelon.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int w;
+    cout<<"Enter your weighat:";
+    cin>>w;
+    if(w%2==0 && w>2)
+    {
+        cout<<"Yes";
+    }
+    else
+    {
+       cout<<"No";
+    }
+     return 0;
+}
