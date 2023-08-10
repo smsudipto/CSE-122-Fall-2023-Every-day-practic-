f16d2049134e4e371ca9a3e3e687666e02ed28e9
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-10 09:16:13 -0700

    Add files via upload

---
+++ advance of parameter .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+void square (int n)
+{
+    int result =n*n;
+    cout << "a="<<result<<endl ;
+
+}
+int main()
+{
+    square(4);
+    square(4);
+    square(4);
+    square(4);
+    square(4);
+    return 0;
+}
