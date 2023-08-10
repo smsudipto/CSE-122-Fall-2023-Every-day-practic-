72a78b2f61dc36b90bfd0109e655b6dc5d7cd462
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-10 09:15:46 -0700

    Add files via upload

---
+++ default value of parameter .cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+
+void display(int a=20,int b=40)
+{
+
+    cout<<"a="<<a<<endl<<"b="<<b<<endl;
+
+}
+int main()
+{
+    display(10 ,10);
+    display(20 ,20);
+    return 0;
+}
+
+
+
