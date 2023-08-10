cfc8f3076e748c3ae56544d363c7c1a8e72b1b54
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-10 09:17:04 -0700

    Add files via upload

---
+++ using function .cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+void addition (int,int );
+{
+ int sum=a+b;
+  cout<<"sum is :"<<sum;
+  return 0;
+}
+
+
+void addition( int a,in b);
+{
+   addition(10,20) ;
+
+}
+
