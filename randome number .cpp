a38790b4358a4b0cb003c05926dbfd3b78fa097d
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2023-08-10 09:17:51 -0700

    Add files via upload

---
+++ randome number .cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+#include<stdlib.h>\\ using hader file "standard library ".
+using namespace std;
+int main()
+{
+    for(int i=0;i<=5;i++)
+    {
+        int result=rand();
+        cout<< "randome number is:"<<result<<endl;
+    }
+}
