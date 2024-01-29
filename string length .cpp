b15bc68c2ca537768d83b61c6866d12bdd20245d
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-01-30 00:19:58 +0600

    Add files via upload

---
+++ string length .cpp
@@ -0,0 +1,15 @@
+#include<bits/stdc++.h>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char s[]="sudipto mandal";
+    int i=0, len=0;
+    while(s[i]!='\0')
+    {
+        i++;
+        len++;
+    }
+
+    cout<<"len="<<len;
+}
