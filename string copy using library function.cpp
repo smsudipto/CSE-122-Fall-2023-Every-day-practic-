4758baca47314aed74f5a686e07f96b17e90f91e
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-01-30 00:20:20 +0600

    Add files via upload

---
+++ string copy using library function.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cstring>
+#include<conio.h>
+using namespace std;
+int main()
+{
+    char source[]="sudipto mandal";
+    char target[30];
+
+   strcpy(target,source);
+
+
+    cout<<"source value="<<source<<endl<<endl;
+    cout<<"target value="<<target;
+    getch();
+
+}
+
