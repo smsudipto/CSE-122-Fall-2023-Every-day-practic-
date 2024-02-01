d4677598f57b57f20b3b5216f827683b56d62d5c
Sudipto Mandal <140314228+smsudipto@users.noreply.github.com>
2024-02-01 21:36:49 +0600

    Add files via upload

---
+++ string concatination.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int main()
+{
+    char str1[]=" I am is ";
+    char str2[]="Sudipto Mandol";
+    strcat(str1,str2);
+    cout<<str1;
+}
